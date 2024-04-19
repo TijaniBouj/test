dfe6888494e9215a7f7655d1f0fb0caa9101c4cc
Tijani <74931826+TijaniBouj@users.noreply.github.com>
2024-04-19 16:27:28 +0100

    Update 1_🏠_Home.py

--- Theme/1_🏠_Home.py
+++ Theme/1_🏠_Home.py
@@ -1,54 +1,41 @@
 import streamlit as st
 import base64
 
-# Define function to set background image with zoom out effect
-def set_bg_with_zoom_out(main_bg):
-    '''
-    A function to set the background image with a zoom out effect.
-
-    Returns
-    -------
-    The background.
-    '''
-    # Set background name
-    main_bg_ext = "bg"
-
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(main_bg.read()).decode()});
-             background-size: contain; /* Zoom out effect */
-             background-repeat: no-repeat;
-             background-position: center;
-         }}
-         .gif-container {{
-             position: absolute;
-             top: 0;
-             left: 0;
-             margin-top: 90px; /* Adjust vertical position as needed */
-             margin-left: 10px; /* Adjust horizontal position as needed */
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
 # Path to the background image
-bg_path = r'bgg.png'
+bg_path = "bgg.png"
 gif_url = "https://media2.giphy.com/media/kLano1oGCDDmlwttPh/giphy-downsized.gif"  # Replace with the URL of your GIF
 
-# Place the GIF in a container with custom CSS for positioning
+# Set background image with zoom out effect
+main_bg_ext = "bg"
+main_bg = open(bg_path, "rb")
+bg_encoded = base64.b64encode(main_bg.read()).decode()
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background: url(data:image/{main_bg_ext};base64,{bg_encoded});
+        background-size: contain; /* Zoom out effect */
+        background-repeat: no-repeat;
+        background-position: center;
+    }}
+    .gif-container {{
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin-top: 90px; /* Adjust vertical position as needed */
+        margin-left: 10px; /* Adjust horizontal position as needed */
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Display GIF
 st.markdown(
     f'<div class="gif-container"><img src="{gif_url}" width="120"></div>',
     unsafe_allow_html=True
 )
 
-# Read the image file
-with open(bg_path, "rb") as bg_file:
-    # Set the background with zoom out effect
-    set_bg_with_zoom_out(bg_file)
-
-# Main content
-st.title("")
-st.sidebar.success("Selected a page above.")
+# Main content (if any)
+st.title("")  # Empty title
+st.sidebar.success("Selected a page above.")  # Sidebar message
